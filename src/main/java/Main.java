06398033b3fa93b9724c3f78ac9a4f4e2d989dc7
AlexFM93 <Alex93FM93@gmail.com>
2023-12-05 13:30:26 +0300

    Fix class "EndFormater".Now it's short and readable.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,7 +9,7 @@ class Main {
         Calculator calculator = new Calculator();
         EndFormater formater = new EndFormater();
         System.out.println("На скольких человек необходимо разделить счёт?");
-        while(!scanner.hasNextInt()) {
+        while (!scanner.hasNextInt()) {
             scanner.next();
             typeErrorMessage();
         }
@@ -22,20 +22,20 @@ class Main {
             System.out.println("Введите название товара:");
             scanner.nextLine();
             String countNames = scanner.nextLine().trim();
-        while (countNames.isEmpty()) {
-            typeErrorMessage();
-            countNames = scanner.nextLine().trim();
-        }
-        System.out.println("Введите цену товара:");
-        while(!scanner.hasNextDouble()) {
-            scanner.next();
-            typeErrorMessage();
-        }
-    double countPrices = scanner.nextDouble();
-    while (countPrices <= 0) {
-        typeErrorMessage();
-        countPrices = scanner.nextDouble();
-}
+            while (countNames.isEmpty()) {
+                typeErrorMessage();
+                countNames = scanner.nextLine().trim();
+            }
+            System.out.println("Введите цену товара:");
+            while (!scanner.hasNextDouble()) {
+                scanner.next();
+                typeErrorMessage();
+            }
+            double countPrices = scanner.nextDouble();
+            while (countPrices <= 0) {
+                typeErrorMessage();
+                countPrices = scanner.nextDouble();
+            }
             calculator.calculate(countNames, countPrices);
             System.out.println("Товар \"" + countNames + "\" успешно добавлен. Хотите добавить ещё один товар? Введите любые символы.\n"
                     + "Или введите \"Завершить\", чтобы завершить процесс добавления товаров.");
@@ -48,10 +48,12 @@ class Main {
             }
         }
     }
+
     static void typeErrorMessage() {
         System.out.println("Это некорректное значение для подсчёта. Попробуйте еще раз.");
     }
 }
+
 class Calculator {
     String names = "";
     double price;
@@ -61,28 +63,25 @@ class Calculator {
         price += inputPrice;
     }
 }
+
 class EndFormater {
-    String rubles = "рубль.";
+    String rubles = "руб.";
+
     String formatRubleEnding(double result) {
-        double ending = Math.floor(result);
-        double lastNumBig = (ending / 100) - (Math.floor(ending / 100));
-        double lastNum = lastNumBig * 100;
-        if (((lastNum >= 0) && (lastNum < 1)) || ((lastNum >= 5) && (lastNum < 21)) || ((lastNum >= 25) && (lastNum <= 30)) || ((lastNum >= 35) && (lastNum <= 40))) {
-            return rubles = "рублей.";
-        } else if (((lastNum >= 1) && (lastNum < 2)) || ((lastNum >= 21) && (lastNum < 22)) || ((lastNum >= 31) && (lastNum < 32)) || ((lastNum >= 41) && (lastNum < 42))) {
-            return rubles = "рубль.";
-        } else if (((lastNum >= 2) && (lastNum < 5)) || ((lastNum >= 22) && (lastNum < 25)) || ((lastNum >= 32) && (lastNum < 35)) || ((lastNum >= 42) && (lastNum < 45))) {
-            return rubles = "рубля.";
-        } else if (((lastNum >= 45) && (lastNum <= 50)) || ((lastNum >= 55) && (lastNum <= 60)) || ((lastNum >= 65) && (lastNum <= 70)) || ((lastNum >= 85) && (lastNum <= 90))) {
-            return rubles = "рублей.";
-        } else if (((lastNum >= 51) && (lastNum < 52)) || ((lastNum >= 61) && (lastNum < 62)) || ((lastNum >= 71) && (lastNum < 72)) || ((lastNum >= 91) && (lastNum < 92)) || ((lastNum >= 81) && (lastNum < 82))) {
-            return rubles = "рубль.";
-        } else if (((lastNum >= 62) && (lastNum < 65)) || ((lastNum >= 72) && (lastNum < 75)) || ((lastNum >= 82) && (lastNum < 85)) || ((lastNum >= 92) && (lastNum < 95))) {
-            return rubles = "рубля.";
-        } else  if (((lastNum >= 95) && (lastNum < 100)) || ((lastNum >= 75) && (lastNum <= 80))) {
+        double roundedEnding = Math.floor(result);
+        double lefts = roundedEnding % 100;
+        double lastDigit = lefts % 10;
+        if (((lefts >= 5) && (lefts <= 20)) || (lefts == 0) ||
+        ((lastDigit >= 5) && (lastDigit <= 9))) {
             return rubles = "рублей.";
         } else {
-            return rubles = "руб.";
+            if ((lefts == 2) || (lefts == 3) || (lefts == 4) ||
+            (lastDigit == 2) || (lastDigit == 3) || (lastDigit == 4)) {
+                return rubles = "рубля.";
+            } else if (((lefts == 1)) || (lastDigit == 1)) {
+                return rubles = "рубль.";
+            }
+            return rubles;
         }
     }
 }
