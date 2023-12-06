f819d45e426062011d800fa2c87b65a74ec4c727
AlexFM93 <Alex93FM93@gmail.com>
2023-12-06 03:42:13 +0300

    Fix scanner validation, now it validate all incorrect inputs.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,7 +4,7 @@ class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         double finalSum;
-        String finalList = "Добавленные товары:%s \n%.2f %s";
+        String finalList = "Добавленные товары:%s \nИтого сумма: %.2f %s на каждого гостя.";
         Calculator calculator = new Calculator();
         EndFormater formater = new EndFormater();
         CountPeople counter = new CountPeople();
@@ -19,20 +19,21 @@ class Main {
                 counter.typeErrorMessage();
                 countNames = scanner.nextLine().trim();
             }
-            System.out.println("Введите цену товара:");
-            while (!scanner.hasNextDouble()) {
-                scanner.next();
-                counter.typeErrorMessage();
-            }
-            double countPrices = scanner.nextDouble();
-            while (countPrices <= 0) {
-                counter.typeErrorMessage();
+
+            double countPrices;
+            do {
+                System.out.println("Введите стоимость товара, большe нуля.");
+                while (!scanner.hasNextDouble()) {
+                    counter.typeErrorMessage();
+                    scanner.next();
+                }
                 countPrices = scanner.nextDouble();
-            }
+            } while (countPrices <= 0);
+
             calculator.calculate(countNames, countPrices);
-            System.out.println("Товар \"" + countNames + "\" успешно добавлен. Хотите добавить ещё один товар? Введите любые символы.\n"
-                    + "Или введите \"Завершить\", чтобы завершить процесс добавления товаров.");
+            System.out.println("Товар \"" + countNames + "\" успешно добавлен. Хотите добавить ещё один товар? Введите любые символы.\n" + "Или введите \"Завершить\", чтобы завершить процесс добавления товаров.");
             String isFinish = scanner.next();
+            scanner.nextLine();
             String finish = "Завершить";
             if (isFinish.equalsIgnoreCase(finish)) {
                 finalSum = calculator.price / countGuests;
@@ -48,7 +49,7 @@ class Calculator {
     double price;
 
     void calculate(String inputName, double inputPrice) {
-        names = names + "\n" + inputName;
+        names = names + "\n" + "\"" + inputName + "\"";
         price += inputPrice;
     }
 }
@@ -60,15 +61,13 @@ class EndFormater {
         double roundedEnding = Math.floor(result);
         double lefts = roundedEnding % 100;
         double lastDigit = lefts % 10;
-        if (((lefts >= 5) && (lefts <= 20)) || (lefts == 0) ||
-                ((lastDigit >= 5) && (lastDigit <= 9))) {
-            return rubles = "рублей.";
+        if (((lefts >= 5) && (lefts <= 20)) || (lefts == 0) || ((lastDigit >= 5) && (lastDigit <= 9))) {
+            return rubles = "рублей";
         } else {
-            if ((lefts == 2) || (lefts == 3) || (lefts == 4) ||
-                    (lastDigit == 2) || (lastDigit == 3) || (lastDigit == 4)) {
-                return rubles = "рубля.";
+            if ((lefts == 2) || (lefts == 3) || (lefts == 4) || (lastDigit == 2) || (lastDigit == 3) || (lastDigit == 4)) {
+                return rubles = "рубля";
             } else if (((lefts == 1)) || (lastDigit == 1)) {
-                return rubles = "рубль.";
+                return rubles = "рубль";
             }
             return rubles;
         }
@@ -83,16 +82,15 @@ class CountPeople {
     }
 
     int countPerson(int input) {
-        System.out.println("На скольких человек необходимо разделить счёт?");
-        while (!scanner.hasNextInt()) {
-            scanner.next();
-            typeErrorMessage();
-        }
-        input = scanner.nextInt();
-        while (input <= 1) {
-            typeErrorMessage();
+        do {
+            System.out.println("На скольких человек необходимо разделить счёт? " + "Введите положительное целое число, больше 1.");
+            while (!scanner.hasNextInt()) {
+                typeErrorMessage();
+                scanner.next();
+            }
             input = scanner.nextInt();
-        }
+        } while (input <= 0);
         return input;
     }
 }
+
